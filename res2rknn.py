ac6c2d5584643355f769ed2be3bdf8a4eb8b84f4
manu <zxthz@126.com>
2024-01-19 18:14:41 +0800

    yolov6 to offline img
    good

--- res2rknn.py
+++ res2rknn.py
@@ -13,7 +13,7 @@ def set_logging():
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path_in',
-                        default='/media/manu/data/docs/sigmastar/Tiramisu_DLS00V010-20220107/ipu/SGS_IPU_SDK_vQ_0.1.0/log/output/unknown_yolov5s_relu_fixed.sim_sgsimg.img_bus.bmp.txt')
+                        default='/media/manu/data/sdks/sigmastar/Tiramisu_DLS00V010-20220107/ipu/SGS_IPU_SDK_vQ_0.1.0/log/output/unknown_acfree_320_fixed.sim_sgsimg.img_students_lt_320.bmp.txt')
     parser.add_argument('--dir_out', default='/home/manu/tmp')
     return parser.parse_args()
 
@@ -29,6 +29,9 @@ def run(args):
             logging.info(lines[i])
             tensor_name, _ = lines[i].strip().split()
             logging.info(tensor_name)
+            while 'Original shape:' not in lines[i]:
+                i += 1
+            tensor_shape_str = lines[i].strip().split(':')[-1][1:-1]
             while 'tensor data:' not in lines[i]:
                 i += 1
             i += 1
@@ -39,16 +42,18 @@ def run(args):
                 tensor_data.extend(line_lst)
                 i += 1
             i += 1
-            db[tensor_name] = tensor_data
+            db[tensor_name + ';' + tensor_shape_str] = tensor_data
         i += 1
     # logging.info(db)
     for key in db.keys():
         data = np.array(db[key]).astype('float')
-        path_out = os.path.join(args.dir_out, key + '.txt')
+        tensor_name = key.split(';')[0]
+        path_out = os.path.join(args.dir_out, tensor_name + '.txt')
         # np.save(path_out, data)
-        c = 255
+        tensor_shape_str = key.split(';')[-1].split()
+        c = int(tensor_shape_str[-1])
         wh = int((len(data) / c) ** 0.5)
-        logging.info((1, wh, wh, c))
+        logging.info((1, wh, wh, c, tensor_name))
         data = np.transpose(data.reshape((1, wh, wh, c)), (0, 3, 1, 2))
         np.savetxt(path_out, data.flatten(), fmt="%f", delimiter="\n")
 
