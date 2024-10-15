cc0e4ac48a03cf167d096d083d1e98a080aaddb4
huangzhe <huangzhe@jbufa.com>
2024-10-15 14:50:09 +0800

    yolov9 model deploy with cos sim > 0.999 using v1.2.9 for quantization & v1.2.3 for simulation

---
+++ res_cvt_yolov9.py
@@ -0,0 +1,68 @@
+import argparse
+import logging
+import os.path
+
+import numpy as np
+
+
+def set_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_in',
+                        default='/media/manu/ST2000DM005-2U91/sdks/sigmastar/Tiramisu_DLS00V010-20220107/jb_sgs_ipu_sdk/SGS_IPU_SDK_v1.2.3/SGS_IPU_SDK_v1.2.3/log/output/unknown_yolov9_smoke_fixed.sim_sgsimg.img_smoke.bmp.txt')
+    parser.add_argument('--dir_out', default='/home/manu/tmp')
+    return parser.parse_args()
+
+
+def run(args):
+    logging.info(args)
+    with open(args.path_in, 'r') as f:
+        lines = f.readlines()
+    i = 0
+    db = dict()
+    while i < len(lines):
+        if 'Tensor:' in lines[i]:
+            logging.info(lines[i])
+            tensor_name, _ = lines[i].strip().split()
+            logging.info(tensor_name)
+            while 'Original shape:' not in lines[i]:
+                i += 1
+            tensor_shape_str = lines[i].strip().split(':')[-1][1:-1]
+            while 'tensor data:' not in lines[i]:
+                i += 1
+            i += 1
+            tensor_data = list()
+            while '}' not in lines[i]:
+                line_lst = lines[i].strip().split()
+                # logging.info(line_lst)
+                tensor_data.extend(line_lst)
+                i += 1
+            i += 1
+            db[tensor_name + ';' + tensor_shape_str] = tensor_data
+        i += 1
+    # logging.info(db)
+    for key in db.keys():
+        data = np.array(db[key]).astype('float')
+        tensor_name = key.split(';')[0]
+        path_out = os.path.join(args.dir_out, tensor_name + '.txt')
+        # np.save(path_out, data)
+        tensor_shape_str = key.split(';')[-1].split()
+        wh = int(tensor_shape_str[-1])
+        c = int(tensor_shape_str[-2])
+        logging.info((1, c, wh, tensor_name))
+        # data = np.transpose(data.reshape((1, wh, wh, c)), (0, 3, 1, 2))
+        np.savetxt(path_out, data.flatten(), fmt="%f", delimiter="\n")
+
+
+def main():
+    set_logging()
+    args = parse_args()
+    run(args)
+
+
+if __name__ == '__main__':
+    main()
